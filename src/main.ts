0119128d8e8d5694528c93c56346018c976bfa30
Sébastien <sbages.dev@gmail.com>
2021-07-17 18:09:28 +0200

    Add express server

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import express, {Request, Response} from "express";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req: Request, res: Response) => res.send("Hello World!"));
+app.listen(port, () => console.log(`listen on http://localhost:${port}/`));
